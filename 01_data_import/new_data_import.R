630186b70227a7dc992538191783590ba3909ab7
hcopley <hgcopley@gmail.com>
2026-02-06 00:32:20 -0500

    Completed data profiling

--- 01_data_import/new_data_import.R
+++ 01_data_import/new_data_import.R
@@ -1,6 +1,6 @@
 library(tidyverse)
-#library(revtools)
-library(synthesisr)
+library(revtools)
+#library(synthesisr)
 library(skimr)
 
 # Folder path
@@ -8,9 +8,10 @@ input <- '00_data_input/'
 output <- '04_output/'
 
 #read in a single .ris file to test  
-df <- read_ref(
-    paste0(input, '20190401_20200131.ris')
-)
+#df <- read_bibliography(
+#    paste0(input, '20180701_20190331.ris')
+#)
+
 
 #get the names of all of the new data files (ending with .ris)
 new_data_files <- list.files(path = input, pattern = '.ris', full.names = TRUE)
@@ -18,17 +19,17 @@ new_data_files <- list.files(path = input, pattern = '.ris', full.names = TRUE)
 #read in all of the new data files and combine into a single data frame
 #keep the file name as a column for reference
 new_data <- new_data_files %>%
-    set_names(basename(.)) %>%
-    map(read_ref) %>%
-    enframe(name = 'file') %>%
-    unnest(cols = c(value))
+    map(read_bibliography) %>%
+    reduce(bind_rows)
 
-
-#get the summary information for the new data
-data_sum <- skim(new_data)
 
 write.csv(new_data, 
           paste0(output, 
                  'combined_new_data.csv'), 
           row.names = FALSE)
+
+#get the summary information for the new data
+data_sum <- skim(new_data)
+
+  
 
